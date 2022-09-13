a9693ac8f69db8c0e74386ef4231dc005791cbb8
ferdyfebriyanto <ferdyfebriyanto22@gmail.com>
2022-09-13 07:56:41 +0700

    Display Map

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:proj4dart/proj4dart.dart';
+// import 'package:latlong/latlong.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,6 +29,34 @@ class MapApp extends StatefulWidget {
 class _MapAppState extends State<MapApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        FlutterMap(
+          options: MapOptions(
+            onTap: (tapPosition, point) => print(tapPosition),
+            center: LatLng(51.5, -0.09),
+            zoom: 13.0,
+          ),
+          layers: [
+            TileLayerOptions(
+              urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              subdomains: ['a', 'b', 'c'],
+            ),
+            MarkerLayerOptions(markers: [
+              Marker(
+                width: 100.0,
+                height: 100.0,
+                point: LatLng(51.5, -0.09),
+                builder: (ctx) => const Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                  size: 40.0,
+                ),
+              ),
+            ]),
+          ],
+        ),
+      ],
+    );
   }
 }
